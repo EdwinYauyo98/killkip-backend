c83032f415d819f0bbd601c9c6eed4a779b81e14
Edwin <eyauyos@uni.pe>
2021-08-19 22:29:34 -0500

    server config

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const config = require('./config');
 const app = express();
 
+app.set('port', process.env.PORT || 3000);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
@@ -36,6 +37,6 @@ app.get('/', function (req, res) {
 ////
 
 
-app.listen(config.port, () => {
+app.listen(app.get('port'), () => {
   console.log('App listening on url http://localhost:' + config.port);
 });
