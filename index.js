97686a822fb56df49e0072b502f5130e9c2c9c52
Edwin <eyauyos@uni.pe>
2021-08-19 22:46:26 -0500

    a

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const config = require('./config');
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
